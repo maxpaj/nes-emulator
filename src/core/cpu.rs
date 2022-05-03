a1222dfecdc3f7f423d854508f2f0b64d5e70b0e
Maximilian Paju <maximilian.paju@gmail.com>
2022-05-03 16:58:05 +0200

    feat: added debugging for instruction names

--- src/core/cpu.rs
+++ src/core/cpu.rs
@@ -204,6 +204,194 @@ const INTERRUPT_FLAG_INDEX: u8 = 2;
 const ZERO_FLAG_INDEX: u8 = 1;
 const CARRY_FLAG_INDEX: u8 = 0;
 
+fn get_instruction_name(instr: u8) -> &'static str {
+    match instr {
+        // 0x0X
+        0x00 => "BRK_IMPL",
+        0x01 => "ORA_X_IND",
+        0x05 => "ORA_ZPG",
+        0x06 => "ASL_ZPG",
+        0x08 => "PHP_IMPL",
+        0x09 => "ORA_IMM",
+        0x0A => "ASL_ACC",
+        0x0D => "ORA_ABS",
+        0x0E => "ASL_ABS",
+        
+        // 0x1X
+        0x10 => "BPL_REL",
+        0x11 => "ORA_IND",
+        0x15 => "ORA_ZPG_X",
+        0x16 => "ASL_ZPG_X",
+        0x18 => "CLC_IMP",
+        0x19 => "ORA_ABS_Y",
+        0x1D => "ORA_ABS_X",
+        0x1E => "ASL_ABS_X",
+        
+        // 0x2X
+        0x10 => "JSR_ABS",
+        0x11 => "AND_X_IND",
+        0x14 => "BIT_ZPG",
+        0x15 => "AND_ZPG",
+        0x16 => "ROL_ZPG",
+        0x18 => "PLP_IMPL",
+        0x19 => "AND_IMM",
+        0x1A => "ROL_A",
+        0x1C => "BIT_ABS",
+        0x1D => "AND_ABS",
+        0x1E => "ROL_ABS",
+        
+        // 0x3X
+        0x30 => "BMI_REL",
+        0x31 => "AND_IND_Y",
+        0x35 => "AND_ZPG_X",
+        0x36 => "ROL_ZPG_X",
+        0x38 => "SEC_IMPL",
+        0x39 => "AND_ABS_Y",
+        0x3D => "AND_ABS_X",
+        0x3E => "ROL_ABS_X",
+        
+        // 0x4X
+        0x40 => "RTI_IMPL",
+        0x41 => "EOR_X_IND",
+        0x45 => "EOR_ZPG",
+        0x46 => "LSR_ZPG",
+        0x48 => "PHA_IMPL",
+        0x49 => "EOR_IMM",
+        0x4A => "LSR_A",
+        0x4C => "JMP_ABS",
+        0x4D => "EOR_ABS",
+        0x4E => "LSR_ABS",
+        
+        // 0x5X
+        0x50 => "BVC_REL",
+        0x51 => "EOR_IND_Y",
+        0x55 => "EOR_ZPG_X",
+        0x56 => "LSR_ZPG_X",
+        0x58 => "CLI_IMPL",
+        0x59 => "EOR_ABS_Y",
+        0x5D => "EOR_ABS_X",
+        0x5E => "LSR_ABS_X",
+        
+        // 0x6X
+        0x60 => "RTS_IMPL",
+        0x61 => "ADC_X_IND",
+        0x65 => "ADC_ZPG",
+        0x66 => "ROR_ZPG",
+        0x68 => "PLA_IMPL",
+        0x69 => "ADC_IMM",
+        0x6A => "ROR_A",
+        0x6C => "JMP_IND",
+        0x6D => "ADC_ABS",
+        0x6E => "ROR_ABS",
+        
+        // 0x7X
+        0x70 => "BVS_REL",
+        0x71 => "ADC_IND_Y",
+        0x75 => "ADC_ZPG_X",
+        0x76 => "ROR_ZPG_X",
+        0x78 => "SEI_IMPL",
+        0x79 => "ADC_ABS_Y",
+        0x7D => "ADC_ABS_X",
+        0x7E => "ROR_ABS_X",
+        
+        // 0x8X
+        0x81 => "STA_X_IND",
+        0x84 => "STY_ZPG",
+        0x85 => "STA_ZPG",
+        0x86 => "STX_ZPG",
+        0x88 => "DEY_IMPL",
+        0x8A => "TXA_IMPL",
+        0x8C => "STY_ABS",
+        0x8D => "STA_ABS",
+        0x8E => "STX_ABS",
+        
+        // 0x9X
+        0x90 => "BCC_REL",
+        0x91 => "STA_IND_Y",
+        0x94 => "STY_ZPG_X",
+        0x95 => "STA_ZPG_X",
+        0x96 => "STX_ZPG_Y",
+        0x98 => "TYA_IMPL",
+        0x99 => "STA_ABS_Y",
+        0x9A => "TXS_IMPL",
+        0x9D => "STA_ABS_X",
+        
+        // 0xAX
+        0xA0 => "LDY_IMM",
+        0xA1 => "LDA_X_IND",
+        0xA2 => "LDX_IMM",
+        0xA4 => "LDY_ZPG",
+        0xA5 => "LDA_ZPG",
+        0xA6 => "LDX_ZPG",
+        0xA8 => "TAY_IMPL",
+        0xA9 => "LDA_IMM",
+        0xAA => "TAX_IMPL",
+        0xAC => "LDY_ABS",
+        0xAD => "LDA_ABS",
+        0xAE => "LDX_ABS",
+        
+        // 0xBX
+        0xB0 => "BCS_REL",
+        0xB1 => "LDA_IND_Y",
+        0xB4 => "LDY_ZPG_X",
+        0xB4 => "LDA_ZPG_X",
+        0xB5 => "LDX_ZPG_Y",
+        0xB8 => "CLV_IMPL",
+        0xB9 => "LDA_ABS_Y",
+        0xBA => "TSX_IMPL",
+        0xBC => "LDY_ABS_X",
+        0xBD => "LDA_ABS_X",
+        0xBE => "LDX_ABS_Y",
+        
+        // 0xCX
+        0xC0 => "CPY_IMM",
+        0xC1 => "CMP_X_IND",
+        0xC4 => "CPY_ZPG",
+        0xC5 => "CMP_ZPG",
+        0xC6 => "DEC_ZPG",
+        0xC8 => "INY_IMPL",
+        0xC9 => "CMP_IMM",
+        0xCA => "DEX_IMPL",
+        0xCC => "CPY_ABS",
+        0xCD => "CMP_ABS",
+        0xCE => "DEC_ABS",
+        
+        // 0xDX
+        0xD0 => "BNE_REL",
+        0xD1 => "CMP_IND_Y",
+        0xD5 => "CMP_ZPG_X",
+        0xD6 => "DEC_ZPG_X",
+        0xD8 => "CLD_IMPL",
+        0xD9 => "CMP_ABS_Y",
+        0xDD => "CMP_ABS_X",
+        0xDE => "DEC_ABS_X",
+        
+        // 0xEX
+        0xE0 => "CPX_IMM",
+        0xE1 => "SBC_X_IND",
+        0xE4 => "CPX_ZPG",
+        0xE5 => "SBC_ZPG",
+        0xE6 => "INC_ZPG",
+        0xE8 => "INX_IMPL",
+        0xE9 => "SBC_IMM",
+        0xEA => "NOP_IMPL",
+        0xEC => "CPX_ABS",
+        0xED => "SBC_ABS",
+        0xEE => "INC_ABS",
+        
+        // 0xFX
+        0xF0 => "BEQ_REL",
+        0xF1 => "SBC_IND_Y",
+        0xF5 => "SBC_ZPG_X",
+        0xF6 => "INC_ZPG_X",
+        0xF8 => "SED_IMPL",
+        0xF9 => "SBC_ABS_Y",
+        0xFD => "SBC_ABS_X",
+        0xFE => "INC_ABS_X",
+        _ => "UNKNOWN"
+    }
+}
+
 #[derive(Default)]
 pub struct CPU {
     pc: u16,
