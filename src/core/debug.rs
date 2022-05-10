6c89ab36d06e954f41279e94b01e35314e39f78f
Maximilian Paju <maximilian.paju@gmail.com>
2022-05-10 23:11:02 +0200

    feat: added debugging methods for hexadecimal numbers

--- src/core/debug.rs
+++ src/core/debug.rs
@@ -1,21 +1,41 @@
-pub fn print_bytes_table(bytes: Vec<u8>, start: usize, end: usize, chunk_size: usize) {
+pub fn print_bytes_table(bytes: &Vec<u8>, start: usize, end: usize, chunk_size: usize) {
     let range: Vec<_> = (start..end + 1).collect();
 
     for chunk in range.chunks(chunk_size) {
         for address in chunk {
-            print!("{:>width$} ", format!("{:x}", address), width=6);
+            eprint!("{:>width$} ", format!("{:x}", address), width = 6);
         }
-        
-        print!("\n");
-        
+
+        eprint!("\n");
+
         for address in chunk {
-            print!("{:>width$} ", format!("{:x}", bytes[*address as usize]), width=6);
+            eprint!(
+                "{:>width$} ",
+                to_hex_u16(bytes[*address as usize] as u16),
+                width = 6
+            );
         }
-        
-        print!("\n\n");
+
+        eprint!("\n\n");
     }
 }
 
-pub fn disassemble(bytes: Vec<u8>, start: usize, end: usize) {
-    //  print("{}");
+pub fn to_hex_u8(x: u8) -> String {
+    return format!("{:x}", x).to_uppercase();
+}
+
+pub fn to_hex_u16(x: u16) -> String {
+    return format!("{:x}", x).to_uppercase();
+}
+
+pub fn to_hex_u32(x: u32) -> String {
+    return format!("{:x}", x).to_uppercase();
+}
+
+pub fn to_binary_u8(x: u8) -> String {
+    return format!("{:#010b}", x);
+}
+
+pub fn to_binary_u16(x: u16) -> String {
+    return format!("{:#018b}", x);
 }
