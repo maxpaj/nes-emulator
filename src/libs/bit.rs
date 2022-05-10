f1c2e8e6bb6a82aa52ae2fb944266552e56e0ff7
Maximilian Paju <maximilian.paju@gmail.com>
2022-05-10 23:13:07 +0200

    refactor: formatting, fixed parameter names

--- src/libs/bit.rs
+++ src/libs/bit.rs
@@ -1,4 +1,3 @@
-
 pub fn clear_bit(u: &mut u8, bit: u8) -> () {
     *u &= !(0b00000001 << bit);
 }
@@ -19,23 +18,21 @@ pub fn toggle_bit(u: &mut u8, bit: u8, set: bool) -> () {
     }
 }
 
-
 #[cfg(test)]
 mod bit_tests {
-    use crate::libs::bit::{toggle_bit, check_bit};
-
+    use crate::libs::bit::{check_bit, toggle_bit};
 
     #[test]
     fn test_toggle_bit() {
-        let mut u8: u8 = 0b10000000;
-        toggle_bit(&mut u8, 0, true);
-        assert_eq!(u8, 0b10000001);
+        let mut n: u8 = 0b10000000;
+        toggle_bit(&mut n, 0, true);
+        assert_eq!(n, 0b10000001);
     }
 
     #[test]
     fn test_check_bit() {
-        let u8: u8 = 0b10000000;
-        let set = check_bit( u8, 7);
+        let n: u8 = 0b10000000;
+        let set = check_bit(n, 7);
         assert_eq!(set, true);
     }
 }
